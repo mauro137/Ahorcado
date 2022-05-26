48501957378a183e297cab7834f853ebd7c0027b
mauro <maurolizarriaga2@gmail.com>
2022-05-26 16:04:46 -0300

    solo falta arreglar un problema con variable booleana y dibujar

--- funciones.js
+++ funciones.js
@@ -1,21 +1,69 @@
-var palabra = "palabra";
-var chars = palabra.split("");
-var intentosRestantes = 6;
-var aciertos = chars.length;
-document.addEventListener("keypress", controlador);
+const listaPalabras = ["ARREGLO", "RED", "DATOS", "EDITOR", "BUG", "EVENTO"];
+const letrasUsadas = [];
+n = Math.floor(Math.random() * listaPalabras.length);
+let palabra = listaPalabras[n];
+let arrayChars = palabra.split("");
+let maskedPalabra = palabra.replace(/[A-Z]/g, "_").split("");
+let intentos = 7;
+let aciertos = arrayChars.length;
 
-function controlador(event) {
-  const teclaPresionada = event.key.toLowerCase();
-  for (let index = 0; index < chars.length; index++) {
-    const element = chars[index];
-    if (element === teclaPresionada) {
+document.querySelector(".palabra").innerHTML = maskedPalabra.join(" ");
+document.addEventListener("keypress", main);
+
+
+
+function main(event,charValido) {
+  const teclaPresionada = event.key.toUpperCase();
+  validarLetra(teclaPresionada);
+  if (charValido) {
+    buscarCoincidencia(teclaPresionada);
+    console.log("valido")
+    /* checkIntentos(intentos,aciertos); */
+  } else {
+    console.log("invalido")
+  }
+}
+function validarLetra  (teclaPresionada) {
+  let charValido = true;
+  try {
+    if (teclaPresionada.match(/^[0-9]*$/)) {
+      throw new Error("No se permiten numeros");
+    }
+    if (teclaPresionada.match(/^\W*$/)) {
+      throw new Error("No se permiten caracteres especiales");
+    }
+    if (letrasUsadas.includes(teclaPresionada)) {
+      throw new Error("Ya usaste esa letra -.-");
+    }
+  } catch (error) {
+    alert(error);
+    charValido = false;
+  }
+  return charValido;
+};
+
+function buscarCoincidencia (teclaPresionada) {
+  letrasUsadas.push(teclaPresionada);
+  for (let index = 0; index < arrayChars.length; index++) {
+    const letra = arrayChars[index];
+    if (letra === teclaPresionada) {
+      console.log('acertaste')
+      maskedPalabra.splice(index,1,teclaPresionada)
       aciertos--;
+      
     } else {
+      intentos--;
+      console.log('fallaste')
     }
   }
-  if (chars.includes(teclaPresionada) != true) {
-    intentosRestantes--;
+};
+
+const checkIntentos = (intentos,aciertos) =>{
+  if (condition) {
+    
   }
-  console.log("TIENES", intentosRestantes, "INTENTOS RESTANTES");
-  console.log("DEBES DESCUBRIR", aciertos, "LETRAS MAS PARA GANAR");
+/*  */
+  /* deshabilitar evento al finalizar juego*/
 }
+
+
